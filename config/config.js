73a4459b065ca785c1a84a88dda1d2053f6babad
Patrick875 <kubwimanapatrick@gmail.com>
2025-03-19 23:23:37 +0200

    remote-db and deployment

--- config/config.js
+++ config/config.js
@@ -9,11 +9,13 @@ const config = {
 		dialect: "postgres",
 	},
 	production: {
-		username: "root",
-		password: null,
-		database: "database_production",
-		host: "127.0.0.1",
-		dialect: "mysql",
+		username: "postgres",
+		password: "yHNJhFxPvDlmjloBBLKaPLkmpmWlXnKG",
+		database: "railway",
+		host: "postgres.railway.internal",
+		dialect: "postgres",
+		port: 44515,
+		use_env_variable: "postgresql://postgres:yHNJhFxPvDlmjloBBLKaPLkmpmWlXnKG@ballast.proxy.rlwy.net:44515/railway",
 	},
 };
 
--- models/index.js
+++ models/index.js
@@ -6,12 +6,18 @@ const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.js")[env];
+const dotenv = require("dotenv");
+
+dotenv.config({
+	path: "./.env",
+});
+
+const config = require(__dirname + "/../config/config.js")[env.trim()];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-	sequelize = new Sequelize(process.env[config.use_env_variable], config);
+	sequelize = new Sequelize(process.env.ONLIE_DB_URL, config);
 } else {
 	sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
